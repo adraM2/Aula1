f1891499cf4625473c4c71c73f89d251171b5fae
adraM2 <119797360+adraM2@users.noreply.github.com>
2022-12-07 18:37:13 +0000

    questão 2

---
+++ Q2 main.py
@@ -0,0 +1,9 @@
+hi=int(input("digite a hora inicial em formato 24h: "))
+mi=int(input("digite o minuto inicial: "))
+hf=int(input("digite a hora final: "))
+mf=int(input("digite o minuto final: "))
+h= hf-hi
+h_=h*60
+m=mf-mi
+horastotal=(h_ + m)//60
+print("O jogo durou",horastotal,"horas e",m,"minutos")
